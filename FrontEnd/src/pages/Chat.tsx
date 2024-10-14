70e74ca35420a70f4e78fb547bf28340081d6fb2
Farhod <faha.azizov@gmail.com>
2024-10-14 23:34:03 +0500

    replace useEffect with useMemo

--- FrontEnd/src/pages/Chat.tsx
+++ FrontEnd/src/pages/Chat.tsx
@@ -4,21 +4,16 @@ import ChatsPanel from "../components/ChatsPanel.tsx";
 import MessagesContainer from "../components/MessagesContainer.tsx";
 
 function Chat() {
-  const { userToSend, messages, scrollRef, unReadMessagesCount } =
-    useSocketContext();
+  const { userToSend, messages, scrollRef } = useSocketContext();
 
   useEffect(() => {
     if (scrollRef.current) {
       scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
     }
-  }, [messages]);
+  }, [messages, userToSend]);
 
   return (
     <>
-      <div className="messenger-wraper-head">
-        <h2>Сообщения</h2>
-        <span>{unReadMessagesCount.count} непрочитанных</span>
-      </div>
       <div className="messenger-wraper">
         <div
           className={`chats-panel ${userToSend !== "none" ? "panel--active" : "panel--inactive"}`}
